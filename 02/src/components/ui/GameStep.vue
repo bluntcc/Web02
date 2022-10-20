97d643252b73a7fcb2d7006b1a80f7d155615020
SuperGyn <709455638@qq.com>
2022-10-20 22:26:42 +0800

    添加下一步按钮

--- 02/src/components/ui/GameStep.vue
+++ 02/src/components/ui/GameStep.vue
@@ -1,6 +1,7 @@
 <template>
   <el-steps :active="active" align-center>
       <el-step :title= "gameStepInfo" :description="gameStepDesList[gameStepInfoIndex]" :key="gameStepInfoIndex" v-for="(gameStepInfo,gameStepInfoIndex) in gameStepInfoList"></el-step>
+      <el-button @click="handleClick">下一步</el-button>
   </el-steps>
 </template>
 
@@ -19,6 +20,11 @@ export default {
     active: {
       type: Number,
       default: 0
+    }
+  },
+  methods: {
+    handleClick () {
+      this.active = this.active < 2 ? this.active + 1 : 0
     }
   }
 }
