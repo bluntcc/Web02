cec493799e7139aceb9445bc3ad0122042b02bc4
SuperGyn <709455638@qq.com>
2022-10-23 15:57:52 +0800

    添加单选框

---
+++ 02/src/components/ui/SingleBox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-button type="text" @click="OpenSingleBoxDialog">点击打开单选框</el-button>
+    <el-dialog :title="title" :visible.sync="dialogSingleBoxVisible" :destroy-on-close="true" :width="boxWidth">
+      <el-empty v-if="dialogSingleBoxVisible" :description="question" :image="imageURL"></el-empty>
+      <el-radio-group v-model="radio">
+        <el-radio v-for="item in singleBoxList" :key="item">{{item}}</el-radio>
+      </el-radio-group>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'textDescDialog',
+  props: {
+    question: {
+      type: String,
+      default: 'no question'
+    },
+    singleBoxList: {
+      type: Array,
+      default: () => []
+    },
+    correctAnswer: {
+      type: String,
+      default: 'no question'
+    },
+    imageURL: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    OpenSingleBoxDialog () {
+      this.dialogSingleBoxVisible = true
+    }
+  },
+  data () {
+    return {
+      dialogSingleBoxVisible: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
